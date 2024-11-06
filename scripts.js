620af5828a851967a9fb3adf21f0fded701456e2
Lluis <lluismut@gmail.com>
2024-11-06 19:00:22 +0100

    bones

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+function toggleNav() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('visible');
+}
+
+// Manejo del formulario de contacto
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     document.getElementById('formMessage').textContent = '¡Mensaje enviado exitosamente!';
